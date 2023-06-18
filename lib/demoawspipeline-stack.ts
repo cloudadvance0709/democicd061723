e63e7715b5c47995517711a531a2ee6fb00c21d8
Chi Lee <cloudadvance0709@gmail.com>
2023-06-18 00:10:13 -0700

    fixed nested git

--- lib/demoawspipeline-stack.ts
+++ lib/demoawspipeline-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { PipelineAppStage } from './demoawspipeline-app-stack';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 
 export class DemoawspipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -21,6 +23,16 @@ export class DemoawspipelineStack extends cdk.Stack {
           'npx cdk synth',
         ],
       }),
-    })
+    });
+
+    const testingStage = democicdpipeline.addStage(new PipelineAppStage(this, 'test', {
+      env: { account: '776733860136', region: 'us-west-2'}
+    }));
+
+    testingStage.addPost(new ManualApprovalStep('approval'));
+
+    const prodStage = democicdpipeline.addStage(new PipelineAppStage(this, 'prod', {
+      env: { account: '776733860136', region: 'us-west-2'}
+    }));
   }
 }
